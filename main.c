f0514c7dbd4f489a40b7797412d2b06f443577f0
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2023-11-17 04:58:53 +0200

    omg

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		write(2, "USAGE: monty file\n", 17);
+		write(2, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(av[1], "r");
