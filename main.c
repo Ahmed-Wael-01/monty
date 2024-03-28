8c51095bf241cc2d0ad30710c1046a18bda1f5a4
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-03-29 01:48:16 +0200

    try

--- main.c
+++ main.c
@@ -18,13 +18,13 @@ int main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(av[1], "r");
 	if (fd == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		printf("Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	fseek(fd, 0, SEEK_END);
@@ -33,7 +33,7 @@ int main(int ac, char **av)
 	buf = malloc(sizeof(char) * (words + 1));
 	if (buf == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
+		printf("Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	fread(buf, sizeof(char), words, fd);
